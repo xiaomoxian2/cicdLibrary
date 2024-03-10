aa4cb80e774fca20398ae812baac4857d7987af0
wangjin <18071453217@163.com>
2024-03-10 16:44:00 +0800

    1

--- 2-cicd-project.sh
+++ 2-cicd-project.sh
@@ -2,9 +2,9 @@
 projectName=$1
 branchName=$2
 deployMode=$3
-k3sIp=$4
+nodeIp=$4
 
-echo -e "项目：${projectName}\n分支：${branchName}\n部署模式：${deployMode}\n部署节点：${k3sIp}"
+echo -e "项目：${projectName}\n分支：${branchName}\n部署模式：${deployMode}\n部署节点：${nodeIp}"
 
 workHome=/home/admin
 cicdHome=/home/admin/cicd
@@ -21,20 +21,20 @@ fi
 function deployMachine(){
     echo "==================物理机方式-部署开始====================="
     cd $tmpDir && /usr/bin/git clone -b ${branchName} $gitUrl
-    bash $cicdHome/${projectName}/2-deploy-m.sh $tmpDir ${projectName} ${branchName} $k3sIp
+    bash $cicdHome/${projectName}/2-deploy-m.sh $tmpDir ${projectName} ${branchName} $nodeIp
     echo "==================物理机方式-部署结束====================="
 }
 
 function deployDocker(){
     echo "==================Docker方式-部署开始====================="
-    scp $workHome/${projectName}/2-deploy-docker.sh $k3sIp:/home/admin/2-deploy-docker.sh
-    ssh $k3sIp "bash /home/admin/2-deploy-docker.sh ${projectName} ${branchName}"
+    scp $workHome/${projectName}/2-deploy-docker.sh $nodeIp:/home/admin/2-deploy-docker.sh
+    ssh $nodeIp "bash /home/admin/2-deploy-docker.sh ${projectName} ${branchName}"
     echo "==================Docker方式-部署结束====================="
 }
 
 function deployK3s(){
     echo "==================K3s方式-部署开始====================="
-    bash $workHome/${projectName}/deploy/2-deploy-k3s.sh ${projectName} ${branchName} $k3sIp
+    bash $workHome/${projectName}/deploy/2-deploy-k3s.sh ${projectName} ${branchName} $nodeIp
     echo "==================K3s方式-部署结束====================="
 }
 
---
+++ py_flask/2-deploy-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+codeDir=$1
+projectName=$2
+branchName=$3
+nodeIp=$4
+# DATE="`date +%Y%m%d%H%M`"
+
+rsync -avz --delete $codeDir/ ${k3sIp}:/home/admin/${projectName}/
+#cd $workHome/tmp && /usr/bin/git clone -b ${branchName} $gitUrl
+ssh $nodeIp "bash supervisorctl restart gunicorn "
+
+[ $? -eq 0 ] && echo "构建成功！"
