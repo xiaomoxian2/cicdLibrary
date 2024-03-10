41e9a7343f01d4c010c6f8232b2ccd8f5b7133d5
wangjin <18071453217@163.com>
2024-03-10 17:24:12 +0800

    1

--- py_flask/2-deploy-docker.sh
+++ py_flask/2-deploy-docker.sh
@@ -6,11 +6,11 @@ projectName=$3
 branchName=$4
 nodeIp=$5
 # DATE="`date +%Y%m%d%H%M`"
+cd ${codeDir}/${projectName}
 imgTag=`/usr/bin/git rev-parse --short HEAD`
 imgName="local/flask:${imgTag}"
 
 cp -f ${cicdHome}/${projectName}/* ${codeDir}/${projectName}/deploy/
-cd ${codeDir}/${projectName}
 /usr/bin/docker build -t ${imgName} -f deploy/Dockerfile_2 .
 /usr/bin/docker tag ${imgName} 10.50.2.92:8086/${imgName}
 /usr/bin/docker push 10.50.2.92:8086/${imgName}
