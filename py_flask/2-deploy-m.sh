ccd9da0d18116c9931824983792b1d5d942620c5
wangjin <18071453217@163.com>
2024-03-10 16:49:37 +0800

    3

--- py_flask/2-deploy-m.sh
+++ py_flask/2-deploy-m.sh
@@ -6,7 +6,7 @@ branchName=$3
 nodeIp=$4
 # DATE="`date +%Y%m%d%H%M`"
 
-rsync -avz --delete $codeDir/ ${k3sIp}:/home/admin/${projectName}/
+rsync -avz --delete $codeDir/ ${nodeIp}:/home/admin/${projectName}/
 #cd $workHome/tmp && /usr/bin/git clone -b ${branchName} $gitUrl
 ssh $nodeIp "bash supervisorctl restart gunicorn "
 
