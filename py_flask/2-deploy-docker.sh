c831efa6618ffa4e9d59ae5b330e8d674345fef6
wangjin <18071453217@163.com>
2024-03-11 08:56:36 +0800

    3

--- py_flask/2-deploy-docker.sh
+++ py_flask/2-deploy-docker.sh
@@ -8,16 +8,17 @@ nodeIp=$5
 # DATE="`date +%Y%m%d%H%M`"
 cd ${codeDir}/${projectName}
 imgTag=`/usr/bin/git rev-parse --short HEAD`
-imgName="local/flask:${imgTag}"
+imgName="cicd/flask:${imgTag}"
 
-cp -f ${cicdHome}/${projectName}/* ${codeDir}/${projectName}/deploy/
-/usr/bin/docker build -t ${imgName} -f deploy/Dockerfile_2 .
-/usr/bin/docker tag ${imgName} 10.50.2.92:8086/demo/${imgName}
-/usr/bin/docker push 10.50.2.92:8086/demo/${imgName}
+cd ..
+cp ${cicdHome}/${projectName}/* .
+/usr/bin/docker build -t ${imgName} -f Dockerfile_2 .
+/usr/bin/docker tag ${imgName} 10.50.2.92:8086/${imgName}
+/usr/bin/docker push 10.50.2.92:8086/${imgName}
 
 #rsync -avz --delete $codeDir/ ${nodeIp}:/home/admin/${projectName}/
 #cd $workHome/tmp && /usr/bin/git clone -b ${branchName} $gitUrl
-ssh $nodeIp "docker ps -a |grep "local/flask" |awk '{print $1}' |xargs docker stop"
-ssh $nodeIp "docker pull 10.50.2.92:8086/demo/${imgName} && docker tag 10.50.2.92:8086/demo/${imgName} ${imgName} && docker run -p 8088:80 -d ${imgName}"
+ssh $nodeIp "docker ps -a |grep "cicd/flask" |awk '{print $1}' |xargs docker stop"
+ssh $nodeIp "docker pull 10.50.2.92:8086/${imgName} && docker tag 10.50.2.92:8086/${imgName} ${imgName} && docker run -p 8088:80 -d ${imgName}"
 
 [ $? -eq 0 ] && echo "构建成功！"
